b3b2fbb7532fbb1f3d6aaf61f6a5d38b63df56a2
rwxrob <rob@rwx.gg>
2022-04-24 11:34:13 -0400

    Update version

--- help.go
+++ help.go
@@ -18,7 +18,7 @@ import (
 // section of help wanted to be passed as a tab-completable parameter.
 var Cmd = &Z.Cmd{
 	Name:    `help`,
-	Version: `v0.4.0`,
+	Version: `v0.5.0`,
 	Summary: `display help similar to man page format`,
 	Usage:   `[SECTION|all]`,
 	Params: []string{
